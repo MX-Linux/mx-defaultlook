ceb335cd709656691f7d89f050effe4289b11f60
oldgizahub <geoffgigg@hotmail.com>
2016-10-28 00:40:21 -0400

    new .desktop, .pro, and translation files for mx-defaultlook

--- translations/mx-defaultlook_de.ts
+++ translations/mx-defaultlook_de.ts
@@ -1,67 +1,131 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="de_DE">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
 <context>
     <name>defaultlook</name>
     <message>
         <location filename="../defaultlook.ui" line="14"/>
-        <source>defaultlook</source>
-        <translation type="unfinished"></translation>
+        <location filename="../defaultlook.cpp" line="46"/>
+        <location filename="../defaultlook.cpp" line="494"/>
+        <location filename="../defaultlook.cpp" line="501"/>
+        <location filename="../defaultlook.cpp" line="510"/>
+        <source>MX Default Look</source>
+        <translation>MX Default Look</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="27"/>
+        <location filename="../defaultlook.ui" line="32"/>
         <source>Panel Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Optionen für Paneele</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="40"/>
+        <location filename="../defaultlook.ui" line="45"/>
         <source>Theme Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Optionen grafische Darstellung</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="47"/>
+        <location filename="../defaultlook.ui" line="52"/>
         <source>Display panel horizontally</source>
-        <translation type="unfinished"></translation>
+        <translation>Paneele waagerecht</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="54"/>
+        <location filename="../defaultlook.ui" line="59"/>
         <source>Default light theme</source>
-        <translation type="unfinished"></translation>
+        <translation>Standard Farbschema &quot;hell&quot;</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="61"/>
+        <location filename="../defaultlook.ui" line="66"/>
         <source>Display panel vertically</source>
-        <translation type="unfinished"></translation>
+        <translation>Paneele senkrecht</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="68"/>
-        <source>Default dark Theme</source>
-        <translation type="unfinished"></translation>
+        <location filename="../defaultlook.ui" line="73"/>
+        <source>Default dark theme</source>
+        <translation>Standard Farbschema &quot;dunkel&quot;</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="150"/>
+        <location filename="../defaultlook.ui" line="80"/>
+        <source>Fix some website fields when using dark theme</source>
+        <translation>Einige Webseiten Felder korrigieren für das Farbschema &quot;dunkel&quot;</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.ui" line="83"/>
+        <source>Firefox dark theme tweak</source>
+        <translation>Firefox Farbschema &quot;dunkel&quot; Einstellung</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.ui" line="165"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Anwenden</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="176"/>
+        <location filename="../defaultlook.ui" line="191"/>
         <source>Quit application</source>
-        <translation type="unfinished"></translation>
+        <translation>Anwendung beenden</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="179"/>
-        <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <location filename="../defaultlook.ui" line="194"/>
+        <source>Close</source>
+        <translation>Schließen</translation>
     </message>
     <message>
-        <location filename="../defaultlook.ui" line="186"/>
+        <location filename="../defaultlook.ui" line="201"/>
         <source>Alt+N</source>
-        <translation type="unfinished"></translation>
+        <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="../defaultlook.cpp" line="45"/>
-        <source>Default Look</source>
-        <translation type="unfinished"></translation>
+        <location filename="../defaultlook.ui" line="211"/>
+        <source>About</source>
+        <translation>Impressum</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.ui" line="222"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="493"/>
+        <source>About MX Default Look</source>
+        <translation>Impressum für MX Default Look</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="494"/>
+        <source>Version: </source>
+        <translation>Version:</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="495"/>
+        <source>App for quick default ui theme changes</source>
+        <translation>App, um die Voreinstellungen für Farbschema, Grafik usw. einfach zu ändern</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="497"/>
+        <source>Copyright (c) MX Linux</source>
+        <translation>Copyright (c) MX Linux</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="498"/>
+        <source>Cancel</source>
+        <translation>Abbruch</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="499"/>
+        <location filename="../defaultlook.cpp" line="501"/>
+        <source>License</source>
+        <translation>Lizenz</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="517"/>
+        <location filename="../defaultlook.cpp" line="526"/>
+        <source>MX Default Looks</source>
+        <translation>MX grafische Darstellung - Voreinstellung</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="518"/>
+        <source>Finished! Firefox may require a restart for changes to take effect</source>
+        <translation>Fertig! Firefox sollte man neustarten, damit die Änderungen wirksam werden.</translation>
+    </message>
+    <message>
+        <location filename="../defaultlook.cpp" line="527"/>
+        <source>This app is XFCE only</source>
+        <translation>Dieses Programm funktioniert nur in der XFCE desktop Umgebung (nicht: KDE, Gnome ...)</translation>
     </message>
 </context>
 </TS>
--- translations/mx-defaultlook_tr.ts
+++ translations/mx-defaultlook_tr.ts
@@ -1,4 +1,4 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="el" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="tr" version="2.1">
 <context>
     <name>defaultlook</name>
     <message>
@@ -8,62 +8,62 @@
         <location filename="../defaultlook.cpp" line="501"/>
         <location filename="../defaultlook.cpp" line="510"/>
         <source>MX Default Look</source>
-        <translation>Προεπιλογή Desktop του ΜΧ</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="32"/>
         <source>Panel Options</source>
-        <translation>Επιλογές του Panel</translation>
+        <translation>Panel Seçenekleri</translation>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="45"/>
         <source>Theme Options</source>
-        <translation>Επιλογές θέματος</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="52"/>
         <source>Display panel horizontally</source>
-        <translation>Απεικόνιση οριζοντίως το προεπιλεγμένο Panel </translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="59"/>
         <source>Default light theme</source>
-        <translation>προεπιλεγμένο φωτεινό θέμα</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="66"/>
         <source>Display panel vertically</source>
-        <translation>Απεικόνιση οριζοντίως το προεπιλεγμένο Panel </translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="73"/>
         <source>Default dark theme</source>
-        <translation>προεπιλεγμένο σκοτεινό θέμα</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="80"/>
         <source>Fix some website fields when using dark theme</source>
-        <translation>Διόρθωση ορισμένων πεδία ιστοσελίδας όταν χρησιμοποιείται το σκοτεινό θέμα</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="83"/>
         <source>Firefox dark theme tweak</source>
-        <translation>Διόρθωση του σκοτεινού θέματος του Firefox</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="165"/>
         <source>Apply</source>
-        <translation>Εφαρμογή</translation>
+        <translation>Uygula</translation>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="191"/>
         <source>Quit application</source>
-        <translation>Κλείστε την εφαρμογή </translation>
+        <translation>Uygulamadan çık</translation>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="194"/>
         <source>Close</source>
-        <translation>Κλείσιμο</translation>
+        <translation>Kapat</translation>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="201"/>
@@ -73,59 +73,59 @@
     <message>
         <location filename="../defaultlook.ui" line="211"/>
         <source>About</source>
-        <translation>Περί</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.ui" line="222"/>
         <source>Help</source>
-        <translation>Βοήθεια </translation>
+        <translation>Yardım</translation>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="493"/>
         <source>About MX Default Look</source>
-        <translation>Περί MX Default Look</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="494"/>
         <source>Version: </source>
-        <translation>Έκδοση:</translation>
+        <translation>Sürüm</translation>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="495"/>
         <source>App for quick default ui theme changes</source>
-        <translation>Πρόγραμμα για γρήγορη προεπιλογή αλλαγές θέματος</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="497"/>
         <source>Copyright (c) MX Linux</source>
-        <translation>Copyright (c)  MX Linux </translation>
+        <translation>Copyright (c) MX Linux</translation>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="498"/>
         <source>Cancel</source>
-        <translation>Ακύρωση</translation>
+        <translation>İptal</translation>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="499"/>
         <location filename="../defaultlook.cpp" line="501"/>
         <source>License</source>
-        <translation>Άδεια</translation>
+        <translation>Ruhsat</translation>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="517"/>
         <location filename="../defaultlook.cpp" line="526"/>
         <source>MX Default Looks</source>
-        <translation>MX Default Looks</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="518"/>
         <source>Finished! Firefox may require a restart for changes to take effect</source>
-        <translation>Ολοκληρώθηκε! Το Firefox μπορεί να χρειαστεί επανεκκίνηση για να ισχύσουν οι αλλαγές</translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../defaultlook.cpp" line="527"/>
         <source>This app is XFCE only</source>
-        <translation>Η εφαρμογή αυτή είναι μόνο για XFCE</translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
