7a99821101425325b633d3515f144456e0f6a07e
Dolphin Oracle <dolphinoracle@gmail.com>
2016-11-15 14:06:38 -0500

    fixed locale issue

--- defaultlook.cpp
+++ defaultlook.cpp
@@ -111,7 +111,7 @@ void defaultlook::whichpanel()
     // take the first panel we see as default
     QString panel_content;
     QStringList panelIDs;
-    panel_content = runCmd("xfconf-query -c xfce4-panel -p /panels | grep -v Value | grep -v ^$").output;
+    panel_content = runCmd("LC_ALL=en_US.UTF-8 xfconf-query -c xfce4-panel -p /panels | grep -v Value | grep -v ^$").output;
     panelIDs = panel_content.split("\n");
     panel = panelIDs.value(0);
     qDebug() << "panel to use: " << panel;
@@ -121,7 +121,7 @@ void defaultlook::fliptohorizontal()
 {
     QString file_content;
     QStringList pluginIDs;
-    file_content = runCmd("xfconf-query -c xfce4-panel -p /panels/panel-" + panel +"/plugin-ids | grep -v Value | grep -v ^$").output;
+    file_content = runCmd("LC_ALL=en_US.UTF-8 xfconf-query -c xfce4-panel -p /panels/panel-" + panel +"/plugin-ids | grep -v Value | grep -v ^$").output;
     pluginIDs = file_content.split("\n");
     qDebug() << pluginIDs;
 
@@ -255,7 +255,7 @@ void defaultlook::fliptovertical()
 {
     QString file_content;
     QStringList pluginIDs;
-    file_content = runCmd("xfconf-query -c xfce4-panel -p /panels/panel-" + panel +"/plugin-ids | grep -v Value | grep -v ^$").output;
+    file_content = runCmd("LC_ALL=en_US.UTF-8 xfconf-query -c xfce4-panel -p /panels/panel-" + panel +"/plugin-ids | grep -v Value | grep -v ^$").output;
     pluginIDs = file_content.split("\n");
     qDebug() << pluginIDs;
 
