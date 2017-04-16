f986923a904fde29b45d6f6e98c7965880fab820
Dolphin Oracle <dolphinoracle@gmail.com>
2017-04-16 13:13:05 -0400

    cleaned up some messages going to stdout

--- defaultlook/defaultlook.cpp
+++ defaultlook/defaultlook.cpp
@@ -746,7 +746,7 @@ void defaultlook::on_toolButtonXFCEpanelSettings_clicked()
 {
     this->hide();
     system("xfce4-panel --preferences");
-    system("xprop -spy -name Panel");
+    system("xprop -spy -name Panel >/dev/null");
     this->show();
     setupuiselections();
 }
